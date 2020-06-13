62e89b58136d5481eca8c1c095ec5e0f47c6f1e6
Daniel20806 <angus20806@gmail.com>
2020-06-13 15:16:55 +0800

    1.0

--- reviews.py
+++ reviews.py
@@ -1,14 +1,17 @@
 data = []
 count = 0
+
 with open('read.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
 		count += 1
 		if count % 10000 == 0:
 			print(len(data))
-print(line)
-print('123' in data)
-print(data [0])
-print('-------------------')
-print(data [1])
-print(len(data))	
+
+print('檔案讀取完了， 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+average = (sum_len/len(data))
+print('平均有', average, '個字元')
